4653222ce67315340e307914f7417735bd9fd6e3
arturjoshi <arturjoshi.mailbox@gmail.com>
2017-01-08 16:08:57 +0200

    Check for account id

---
+++ trinjerservice/src/main/java/com/arturjoshi/project/dto/ProjectDto.java
@@ -0,0 +1,7 @@
+package com.arturjoshi.project.dto;
+
+/**
+ * Created by arturjoshi on 08-Jan-17.
+ */
+public class ProjectDto {
+}
---
+++ trinjerservice/src/main/java/com/arturjoshi/project/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.arturjoshi.project.repository;
+
+/**
+ * Created by arturjoshi on 08-Jan-17.
+ */
+public interface ProjectRepository {
+}
