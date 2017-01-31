5e60632bae93f9144d11d57f1ccb1f12d919ecfc
Azelenskiy <andrii.zelenskyy@n-cube.co.uk>
2017-01-31 15:30:18 +0200

    Base validation

--- ui/src/app/registration/registration.component.ts
+++ ui/src/app/registration/registration.component.ts
@@ -15,12 +15,38 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class RegistrationDialog{
     user: RegistrationUserDTO;
     registrationForm: FormGroup;
+    formErrors = {
+        'username': '',
+        'email': '',
+        'password': '',
+        'passwordConfirm': ''
+    };
+    validationMessages = {
+        'username': {
+            'required': 'Username is require'
+        },
+        'email': {
+            'required': 'Email is require',
+            'pattern': 'Incorrect email format'
+        },
+        'password': {
+            'required' : 'Password is require'
+        },
+        'confirmPassword': {
+            'required': 'Re-enter password',
+            'incorrect': 'Passwords are not equal'
+        }
+    };
 
     constructor(private dialogRef: MdDialogRef<RegistrationDialog>,
                 private formBuilder: FormBuilder){
 
         this.user = RegistrationUser.getNewRegistrationUser();
         this.registrationForm = this.formBuilder.group(this.getFormGroup());
+
+        this.registrationForm.valueChanges.subscribe(() => {
+            this.onFormChange();
+        })
     }
 
     registration(){
@@ -34,6 +60,23 @@ export class RegistrationDialog{
                 Validators.pattern(/^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i)]],
             'password': [this.user.password, [Validators.required]],
             'confirmPassword': [this.user.passwordConfirm, [Validators.required]]
+        }
+    }
+
+    private onFormChange(){
+        if(!this.registrationForm) return ;
+        const form = this.registrationForm;
+
+        for(const field in this.formErrors){
+            this.formErrors[field] = '';
+            const control = form.get(field);
+
+            if(control && control.dirty && !control.valid){
+                const messages = this.validationMessages[field];
+                for(const key in control.errors){
+                    this.formErrors[field] += messages[key] + " ";
+                }
+            }
         }
     }
 
