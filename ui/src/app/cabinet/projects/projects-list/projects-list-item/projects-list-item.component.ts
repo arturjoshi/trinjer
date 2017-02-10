cd582dbd7d85e6c3b7f64a41899f027106d4685e
Azelenskiy <andrii.zelenskyy@n-cube.co.uk>
2017-02-10 17:52:46 +0200

    Add project list item

---
+++ ui/src/app/cabinet/projects/projects-list/projects-list-item/projects-list-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'project-list-item',
+    templateUrl: 'project-list-item.template.html'
+})
+export class ProjectListItemComponent{}
--- ui/src/app/cabinet/projects/projects.module.ts
+++ ui/src/app/cabinet/projects/projects.module.ts
@@ -1,3 +1,4 @@
+import { ProjectListItemComponent } from './projects-list/projects-list-item/projects-list-item.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateProjectDialog } from './create-project/create-project.dialog';
 import { ProjectRoutingModule } from './project-routing.module';
@@ -25,6 +26,7 @@ import { CommonModule } from '@angular/common';
     ],
     declarations: [
         ProjectsListComponent,
+        ProjectListItemComponent,
         CreateProjectDialog
     ],
     entryComponents: [
