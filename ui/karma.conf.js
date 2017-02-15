0789a739b8b8424ac39c91c3d2b39cfa0ba1c74c
Azelenskiy <andrii.zelenskyy@n-cube.co.uk>
2017-02-15 13:47:00 +0200

    Connect to gulpfile

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -112,7 +112,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome', 'PhantomJS', 'PhantomJS_custom'],
+        browsers: ['PhantomJS_custom'],
         singleRun: false
     })
 };
